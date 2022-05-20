b912b96a910560272123de04608d97adf0b268b1
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-20 20:32:43 +0200

    fixed the issue where Servings number wasn't showing

--- src/js/model.js
+++ src/js/model.js
@@ -16,6 +16,7 @@ export const loadRecipe = async function (id) {
   try {
     const data = await getJSON(`${API_URL}${id}`);
     const { recipe } = data.data;
+    console.log(recipe.servings);
     state.recipe = {
       id: recipe.id,
       title: recipe.title,
@@ -36,6 +37,7 @@ export const loadRecipe = async function (id) {
 export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
+
     const data = await getJSON(`${API_URL}?search=${query}`);
     console.log(data);
 
@@ -45,8 +47,10 @@ export const loadSearchResults = async function (query) {
         title: rec.title,
         publisher: rec.publisher,
         image: rec.image_url,
+        ...(rec.key && { key: rec.key }),
       };
     });
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err}`);
     throw err;
@@ -65,6 +69,8 @@ export const getSearchResultsPage = function (page = state.search.page) {
 export const updateServings = function (newServings) {
   state.recipe.ingredients.forEach(ing => {
     ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    // newQt = oldQt * newServings / oldServings // 2 * 8 / 4 = 4
   });
+
   state.recipe.servings = newServings;
 };
